0efe18e6b7c91b52df975acc35869b9f791f31ad
Aare Undo <aare@carto.com>
2017-01-24 16:01:34 +0200

    html, js, css, plugin references, config update, README update

--- README.md
+++ README.md
@@ -18,6 +18,13 @@ Contains **2** Cordova plugins
 * `cordova restore` – Platforms and plugins are automatically restored from config.xml when the 'cordova prepare' command is run.
 * `cordova run android` & `cordova run ios` – automatically locates an available device and runs the app (prefers real device to emulator/simulator)
 
+###### NB! If installing iOS plugin for the first time, you need to change compiler flags the project's xcode project
+
+Open the workspace file (XCode) under `platforms/ios/HelloCordova/`, open build settings and make the following changes:
+
+  * Set `C++ Language Dialect` to `C++11`
+  * Set `C++ Standard Library` to `libc++`
+  
 ### Sample
 
 `TODO`
--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,4 @@
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -16,6 +17,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -28,19 +30,64 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+
+        // Licenses are platform-specific, need to check the platform before registering
+        // Using device plugin to achieve that. To install the plugin enter:
+        // cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-devic‌​e.git
+
+        if (device.platform == "Android") {
+            console.log("Device is Droid")
+            var license = "XTUN3Q0ZFN21YNnp3bzRicUs4VFVFYWhZMzl4VEphNTFBaFFBZzk5bGc2V3V4RE5MaW1xTTJpZW9NeXhZUXc9PQoKYXBwVG9rZW49NTJiYTQxNWYtNDM3My00NjdjLWJiMjYtMjA3YzI2NzIwZmIxCnBhY2thZ2VOYW1lPWNvbS5jYXJ0by5zZGsuY29yZG92YQpvbmxpbmVMaWNlbnNlPTEKcHJvZHVjdHM9c2RrLWFuZHJvaWQtNC4qCndhdGVybWFyaz1jdXN0b20K";
+        } else {
+            var license = "XTUMwQ0ZBVVI0VFlhM2ZkaDVtUUVLN0RVMHpzeEFGd1lBaFVBdWtiTXAvMzAwOUJJb1BnMFlaekUvSmJkK3VrPQoKYXBwVG9rZW49OWEzNDAzZDAtODE1My00NzU0LTgwYWEtNDE4MDIxYjhlYTk0CmJ1bmRsZUlkZW50aWZpZXI9Y29tLmNhcnRvLnNkay5jb3Jkb3ZhCm9ubGluZUxpY2Vuc2U9MQpwcm9kdWN0cz1zZGstaW9zLTQuKgp3YXRlcm1hcms9Y3VzdG9tCg==";
+            console.log("Device is iOS");
+            // Set top margin to account for the status bar
+            $(".navigation_bar").css("margin-top", "20px");
+        }
+        
+        // License registration. This should be the first step
+        MapView.registerLicense(license);
+        
+        // Passing html element is required for location and size
+        // (standard <div> tag, size and location is set in index.css)
+        var mapView = $(".mapView");
+ 
+        // Initialize map with default base layer
+        MapView.initialize(mapView);
+        
+        // Set click listener
+        MapView.setClickListener(onMapClicked);
     },
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
     }
 };
 
 app.initialize();
+
+function onMapClicked(clickInfo) {
+
+    if (device.platform == "iOS") {
+        // Android is already Json, iOS returns clickInfo as a string
+        clickInfo = $.parseJSON(clickInfo);
+    }
+    
+    var latitude = clickInfo["latitude"];
+    var longitude = clickInfo["longitude"];
+    
+    console.log("?????+++");
+    // Click type can be "SINGLE", "DOUBLE", "DUAL" or "LONG"
+    var clickType = clickInfo["click_type"];
+
+    // alert("Clicked coordinates: " + x + ", " + y + " (type: " + clickType + ")");
+    var title = "Clicked (" + clickType + ")";
+    var description = "Coordinates: " + latitude + ", " + longitude;
+
+    MapView.showPopup(longitude, latitude, title, description);
+}
+
+
+
+
